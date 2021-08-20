c4cd120f45ffbfb87d82dafcb2a1038511b32f8b
Anthony Zhang <anthonyzhang1997@gmail.com>
2021-08-20 19:47:46 -0400

    fixing onclick

--- src/components/PosNeg.js
+++ src/components/PosNeg.js
@@ -1,23 +1,28 @@
-import React from "react";
+import React,{ useState } from "react";
 
 export const PosNeg = () => {
 
-  function checkSign(){
-    console.log('@');
-  }
+	const [type, setType] = useState('');
+
+	function checkSign(type){
+		console.log(type);
+		return <h4>{type} has been selected.</h4>;
+	}
+
 	return (
     
 		<div>
 			<label htmlFor="type">Transaction Type</label>
 			<br />
 			<div className="posneg">
-				<button className="btn" type="income" value='income' onClick={()=> checkSign(this.value)}>
+				<button className="btn" type="income" value='income'  onClick={()=> checkSign('income')}>
 					Income
 				</button>
-				<button className="btn" type="expense" value='expense' onClick={()=> checkSign(this.value)}>
+				<button className="btn" type="expense" value='expense'  onClick={()=> checkSign('expense')}>
 					Expense
 				</button>
 			</div>
+			<div>{checkSign()}</div>
 		</div>
     
 	);
